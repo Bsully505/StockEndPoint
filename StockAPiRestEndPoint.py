a47023ff68bc507a13d86451da5a7eea1d9e0f40
Bryan Sullivan <Bsully505@yahoo.com>
2021-01-11 14:58:14 -0500

    testing github in atom

--- StockAPiRestEndPoint.py
+++ StockAPiRestEndPoint.py
@@ -1,3 +1,5 @@
-if __name__  == '__main__':
-    print("hello World")
-    print("reading file")
+if __name__ == '__main__'{
+    Print("hello world");
+    Print("test");
+    print("last test")
+}
